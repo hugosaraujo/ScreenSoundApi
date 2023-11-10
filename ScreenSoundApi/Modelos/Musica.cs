4dad345f1009a2a57067c1ef08110b379b724cb6
hugo de araujo <hugo.saraujo@tutanota.com>
2023-11-10 10:41:13 -0300

    Criação de classe musica e desserialização de json de musicas

---
+++ ScreenSoundApi/Modelos/Musica.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection.Metadata.Ecma335;
+using System.Text.Json.Serialization;
+using System.Xml;
+
+namespace ScreenSoundApi.Modelos;
+
+internal class Musica
+{
+    [JsonPropertyName("song")]
+    public string? Nome { get; set; }
+    [JsonPropertyName("artist")]
+    public string? Artista { get; set; }
+    [JsonPropertyName("duration_ms")]
+    public int Duracao { get; set; }
+    [JsonPropertyName("genre")]
+    public string? Genero { get; set; }
+
+    public void ExibirDetalhes()
+    {
+        Console.WriteLine($"Nome: {Nome}");
+        Console.WriteLine($"Artista: {Artista}");
+        Console.WriteLine($"Duracao: {Duracao/1000}");
+        Console.WriteLine($"Genero Musical: {Genero}");
+    }
+}
--- ScreenSoundApi/Program.cs
+++ ScreenSoundApi/Program.cs
@@ -1,9 +1,13 @@
-﻿using (HttpClient cliente = new HttpClient())
+﻿using ScreenSoundApi.Modelos;
+using System.Text.Json;
+
+using (HttpClient cliente = new HttpClient())
 {
     try
     {
         string resposta = await cliente.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
-        Console.WriteLine(resposta);
+        var musicas = JsonSerializer.Deserialize<List<Musica>>(resposta)!;
+        musicas[1997].ExibirDetalhes();
     }
     catch (Exception ex)
     {
